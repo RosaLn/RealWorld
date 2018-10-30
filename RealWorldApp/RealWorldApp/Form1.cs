518b23b7ec3bc15aa457fb2a5b0fb054ecf27288
CICLOS\b14-09m <rafacano91@gmail.com>
2018-10-30 14:20:04 +0100

    Pantalla principal añadida

---
+++ RealWorldApp/RealWorldApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealWorldApp
+{
+    public partial class Form1 : Form
+    {
+        private GifImage gifImage = null;
+        private string filePath = @"C:\Users\b14-09m\Documents\Visual Studio 2015\Projects\matrix2.gif";
+        public Form1()
+        {
+            InitializeComponent();
+            gifImage = new GifImage(filePath);
+            gifImage.ReverseAtEnd = false; //dont reverse at end
+            pictureBox1.Image = gifImage.GetNextFrame();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
