19fc759256a03fcfe8c556f3eecf62149dd05379
RosaLn <rosalialeon10@DESKTOP-RPQ8S33>
2018-11-02 17:45:20 +0100

    Letras verdes y botones del form tablero

--- RealWorldApp/RealWorldApp/Tablero.Designer.cs
+++ RealWorldApp/RealWorldApp/Tablero.Designer.cs
@@ -81,7 +81,7 @@
             this.Column4,
             this.Column5});
             this.dgvTablero.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.dgvTablero.Location = new System.Drawing.Point(12, 33);
+            this.dgvTablero.Location = new System.Drawing.Point(12, 30);
             this.dgvTablero.MultiSelect = false;
             this.dgvTablero.Name = "dgvTablero";
             this.dgvTablero.ReadOnly = true;
@@ -138,18 +138,25 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(822, 484);
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(822, 481);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Final";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(822, 174);
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(822, 171);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(169, 161);
             this.richTextBox1.TabIndex = 3;
@@ -158,11 +165,11 @@
             // lblMuertes
             // 
             this.lblMuertes.AutoSize = true;
-            this.lblMuertes.BackColor = System.Drawing.Color.Transparent;
+            this.lblMuertes.BackColor = System.Drawing.Color.Black;
             this.lblMuertes.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblMuertes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMuertes.ForeColor = System.Drawing.Color.Magenta;
-            this.lblMuertes.Location = new System.Drawing.Point(592, 72);
+            this.lblMuertes.ForeColor = System.Drawing.Color.Lime;
+            this.lblMuertes.Location = new System.Drawing.Point(592, 69);
             this.lblMuertes.Name = "lblMuertes";
             this.lblMuertes.Padding = new System.Windows.Forms.Padding(15);
             this.lblMuertes.Size = new System.Drawing.Size(53, 56);
@@ -172,11 +179,11 @@
             // lblRes
             // 
             this.lblRes.AutoSize = true;
-            this.lblRes.BackColor = System.Drawing.Color.Transparent;
+            this.lblRes.BackColor = System.Drawing.Color.Black;
             this.lblRes.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRes.ForeColor = System.Drawing.Color.Magenta;
-            this.lblRes.Location = new System.Drawing.Point(729, 70);
+            this.lblRes.ForeColor = System.Drawing.Color.Lime;
+            this.lblRes.Location = new System.Drawing.Point(729, 67);
             this.lblRes.Name = "lblRes";
             this.lblRes.Padding = new System.Windows.Forms.Padding(15);
             this.lblRes.Size = new System.Drawing.Size(53, 56);
@@ -186,66 +193,66 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Magenta;
-            this.label1.Location = new System.Drawing.Point(589, 33);
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(589, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 32);
+            this.label1.Size = new System.Drawing.Size(49, 30);
             this.label1.TabIndex = 8;
-            this.label1.Text = "SMITH \r\n KILLS:";
+            this.label1.Text = "SMITH \r\nKILLS:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Magenta;
-            this.label2.Location = new System.Drawing.Point(686, 33);
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(708, 30);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 32);
+            this.label2.Size = new System.Drawing.Size(105, 30);
             this.label2.TabIndex = 9;
-            this.label2.Text = "            NEO \r\nRESURRECTIONS:";
+            this.label2.Text = "     NEO \r\nRESURRECTIONS:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Magenta;
-            this.label3.Location = new System.Drawing.Point(628, 204);
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(623, 238);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 24);
+            this.label3.Size = new System.Drawing.Size(32, 18);
             this.label3.TabIndex = 10;
             this.label3.Text = "Neo";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Magenta;
-            this.label4.Location = new System.Drawing.Point(628, 259);
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(623, 293);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 24);
+            this.label4.Size = new System.Drawing.Size(48, 18);
             this.label4.TabIndex = 11;
             this.label4.Text = "Smith";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Magenta;
-            this.label5.Location = new System.Drawing.Point(627, 320);
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(623, 347);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(146, 24);
+            this.label5.Size = new System.Drawing.Size(112, 18);
             this.label5.TabIndex = 12;
             this.label5.Text = "Generic < 30%";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(822, 411);
+            this.progressBar1.Location = new System.Drawing.Point(822, 408);
             this.progressBar1.Maximum = 200;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -257,36 +264,36 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Magenta;
-            this.label6.Location = new System.Drawing.Point(818, 388);
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(818, 385);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(67, 20);
+            this.label6.Size = new System.Drawing.Size(63, 19);
             this.label6.TabIndex = 16;
             this.label6.Text = "Queue:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.Magenta;
-            this.label7.Location = new System.Drawing.Point(628, 361);
+            this.label7.BackColor = System.Drawing.Color.Black;
+            this.label7.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Lime;
+            this.label7.Location = new System.Drawing.Point(623, 391);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(176, 48);
+            this.label7.Size = new System.Drawing.Size(136, 36);
             this.label7.TabIndex = 17;
             this.label7.Text = "Generic between \r\n30% and 50%";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Magenta;
-            this.label8.Location = new System.Drawing.Point(627, 430);
+            this.label8.BackColor = System.Drawing.Color.Black;
+            this.label8.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Lime;
+            this.label8.Location = new System.Drawing.Point(623, 462);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(140, 24);
+            this.label8.Size = new System.Drawing.Size(104, 18);
             this.label8.TabIndex = 18;
             this.label8.Text = "Generic >50%";
             // 
@@ -296,20 +303,25 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(916, 484);
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(916, 481);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 23;
-            this.button2.Text = "Salir";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox5
             // 
             this.pictureBox5.Image = global::RealWorldApp.Properties.Resources.Trump3;
-            this.pictureBox5.Location = new System.Drawing.Point(572, 415);
+            this.pictureBox5.Location = new System.Drawing.Point(576, 445);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.Size = new System.Drawing.Size(41, 39);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 21;
             this.pictureBox5.TabStop = false;
@@ -317,9 +329,9 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::RealWorldApp.Properties.Resources.Trump2;
-            this.pictureBox4.Location = new System.Drawing.Point(572, 359);
+            this.pictureBox4.Location = new System.Drawing.Point(577, 386);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.Size = new System.Drawing.Size(40, 41);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 20;
             this.pictureBox4.TabStop = false;
@@ -327,9 +339,9 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::RealWorldApp.Properties.Resources.Trump1;
-            this.pictureBox3.Location = new System.Drawing.Point(572, 303);
+            this.pictureBox3.Location = new System.Drawing.Point(577, 330);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.Size = new System.Drawing.Size(40, 39);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 19;
             this.pictureBox3.TabStop = false;
@@ -337,9 +349,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::RealWorldApp.Properties.Resources.smith;
-            this.pictureBox2.Location = new System.Drawing.Point(572, 247);
+            this.pictureBox2.Location = new System.Drawing.Point(577, 277);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.Size = new System.Drawing.Size(40, 38);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 14;
             this.pictureBox2.TabStop = false;
@@ -347,9 +359,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::RealWorldApp.Properties.Resources.neo2;
-            this.pictureBox1.Location = new System.Drawing.Point(572, 191);
+            this.pictureBox1.Location = new System.Drawing.Point(577, 223);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(40, 37);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 13;
             this.pictureBox1.TabStop = false;
@@ -367,16 +379,16 @@
             // 
             // circularProgressBar1
             // 
-            this.circularProgressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.circularProgressBar1.BackColor = System.Drawing.SystemColors.Desktop;
             this.circularProgressBar1.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.circularProgressBar1.ForeColor = System.Drawing.Color.Black;
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.Lime;
             this.circularProgressBar1.LineColor = System.Drawing.Color.Silver;
-            this.circularProgressBar1.Location = new System.Drawing.Point(861, 12);
+            this.circularProgressBar1.Location = new System.Drawing.Point(861, 9);
             this.circularProgressBar1.Maximum = ((long)(20));
             this.circularProgressBar1.MinimumSize = new System.Drawing.Size(100, 100);
             this.circularProgressBar1.Name = "circularProgressBar1";
-            this.circularProgressBar1.ProgressColor1 = System.Drawing.Color.Orange;
-            this.circularProgressBar1.ProgressColor2 = System.Drawing.Color.Orange;
+            this.circularProgressBar1.ProgressColor1 = System.Drawing.Color.Lime;
+            this.circularProgressBar1.ProgressColor2 = System.Drawing.Color.Aqua;
             this.circularProgressBar1.ProgressShape = RealWorldApp.CircularProgressBar._ProgressShape.Flat;
             this.circularProgressBar1.Size = new System.Drawing.Size(130, 130);
             this.circularProgressBar1.TabIndex = 5;
